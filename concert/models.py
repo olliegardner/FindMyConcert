f46595629255bf9b57cb324fb3e1142b1d7ffc15
“Axel <a.stjerngr@gmail.com>
2019-02-26 17:12:49 +0000

    Added unfinished database

--- concert/models.py
+++ concert/models.py
@@ -1,6 +1,10 @@
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, User
-
+import datetime
+from datetime import datetime
+from django.utils.translation import gettext as _
+from django.core.validators import MaxValueValidator, MinValueValidator
+from django.utils import timezone
 #class User(AbstractBaseUser):
 	#username = models.CharField(unique=True, max_length=30, verbose_name="Username")
 
@@ -20,13 +24,44 @@ class UserProfile(models.Model):
 
 
 class Venue(models.Model):
-	name = models.CharField(max_length=128) 
-	location = models.CharField(max_length=128) 
-	url = models.URLField()
-	description = models.CharField(max_length=560) 
-	phone_number = models.CharField(max_length=560) 
-	photo = models.ImageField(upload_to='venueImages', blank=True, null=True)
+	venueID      = models.AutoField(primary_key=True)
+	name         = models.CharField(max_length=128) 
+	location     = models.CharField(max_length=128) 
+	url          = models.URLField()
+	description  = models.CharField(max_length=560) 
+	phone_number = models.CharField(max_length=15) 
+	capacity     = models.IntegerField(default=0)
+	photo        = models.ImageField(upload_to='venueImages', blank=True, null=True)
 
-	views = models.IntegerField(default=0)
+	def __str__(self): 
+		return self.name
+
+
+class Concert(models.Model):
+	concertID   = models.AutoField(primary_key=True)
+	venue       = models.ForeignKey(Venue)
+	artist      = models.CharField(max_length=128) 
+	Date        = models.DateField(_("Date"))
+	StartTime   = models.DateTimeField(_("Start Time"))
+	EndTime     = models.DateTimeField(_(u"End Time"))
+	photo       = models.ImageField(upload_to='venueImages', blank=True, null=True)
+	url         = models.URLField()
+	description = models.CharField(max_length=560) 
+
+	def __str__(self): 
+		return self.name
+
+class Comment(models.Model):
+	rating = models.IntegerField( default=5,
+    validators=[
+        MaxValueValidator(5),
+        MinValueValidator(1)
+        ])
+	commentID   = models.AutoField(primary_key=True)
+	concert     = models.ForeignKey(Concert)
+	date        = models.DateField(_("Date"),default=timezone.now)
+	time        = models.DateTimeField(_(u"Comment Time"), default=timezone.now)
+
+
 	def __str__(self): 
 		return self.name
