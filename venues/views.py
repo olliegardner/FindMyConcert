3dcbd009309b6a9e0af7540e4c972c7fad0cb109
“Axel <a.stjerngr@gmail.com>
2019-03-07 17:15:22 +0000

    Actually now we can upload concerts

--- venues/views.py
+++ venues/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from concert.models import Concert
 from venues.forms import ConcertForm
+from django.http import HttpResponse, HttpResponseRedirect
 def venueIndex(request):
     return render(request, 'venues/index.html')
 
@@ -12,16 +13,16 @@ def addConcert(request):
 
         if form.is_valid():
             concert = Concert()
-            concert.artist     = form.cleaned_data['artist']
-            concert.date       = form.cleaned_data['date']
-            concert.start_time = form.cleaned_data['start_time']
-            concert.end_time   = form.cleaned_data['end_time']
-            concert.image      = form.cleaned_data['image']
-            concert.url        = form.cleaned_data['url']
-            concert.venue      = request.user.venue
-            concert.descitpion = form.cleaned_data['descitpion']
+            concert.artist      = form.cleaned_data['artist']
+            concert.date        = form.cleaned_data['date']
+            concert.start_time  = form.cleaned_data['start_time']
+            concert.end_time    = form.cleaned_data['end_time']
+            concert.image       = form.cleaned_data.get('image')
+            concert.url         = form.cleaned_data['url']
+            concert.venue       = request.user.venue
+            concert.description = form.cleaned_data['description']
             concert.save()
 
 
-            return index(venueIndex)
+            return render(request, 'venues/index.html')
     return render(request, 'venues/addConcert.html', {'form': form})
