7cbe5e104e0635b924d1d52d08eb48f1813afc61
“Axel <a.stjerngr@gmail.com>
2019-03-08 09:46:18 +0000

    Custom decorators added

---
+++ FindMyConcert/custom_decorators.py
@@ -0,0 +1,23 @@
+from django.contrib.auth import REDIRECT_FIELD_NAME
+from django.contrib.auth.decorators import user_passes_test
+
+class decorators():
+    def venue_required(function=None, redirect_field_name=REDIRECT_FIELD_NAME, login_url='login'):
+        actual_decorator = user_passes_test(
+            lambda u: u.is_active and u.is_venue,
+            login_url=login_url,
+            redirect_field_name=redirect_field_name
+        )
+        if function:
+            return actual_decorator(function)
+        return actual_decorator
+
+    def giggoer_required(function=None, redirect_field_name=REDIRECT_FIELD_NAME, login_url='login'):
+        actual_decorator = user_passes_test(
+            lambda u: u.is_active and not (u.is_venue),
+            login_url=login_url,
+            redirect_field_name=redirect_field_name
+        )
+        if function:
+            return actual_decorator(function)
+        return actual_decorator
