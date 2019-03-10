01cab0b94834f3fbda546a092f1409f8e4e0da64
Patryk K <patryk.kaczmarczyk.mail@gmail.com>
2019-03-10 14:02:26 +0000

    Cleaned up some imports (i.e. sorted alphabetically)

--- FindMyConcert/urls.py
+++ FindMyConcert/urls.py
@@ -13,11 +13,11 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.contrib import admin, auth
-from django.contrib.auth import views as auth_views
 from django.conf import settings
 from django.conf.urls import url, include
 from django.conf.urls.static import static
+from django.contrib import admin, auth
+from django.contrib.auth import views as auth_views
 from concert import views
 
 urlpatterns = [
--- concert/models.py
+++ concert/models.py
@@ -1,9 +1,10 @@
-from django.db import models
-from django.contrib.auth.models import AbstractUser
 import datetime
-from django.utils.translation import gettext as _
+
+from django.contrib.auth.models import AbstractUser
 from django.core.validators import MaxValueValidator, MinValueValidator
+from django.db import models
 from django.utils import timezone
+from django.utils.translation import gettext as _
 
 class User(AbstractUser):
     email = models.EmailField(max_length=70)
