6be8bfba3c91df4ea3039a56d51c9a0f21c0a640
“Axel <a.stjerngr@gmail.com>
2019-03-10 01:48:36 +0000

    Concerts can now be added to pop script

--- populate.py
+++ populate.py
@@ -5,6 +5,7 @@ import django
 django.setup()
 
 import pandas as pd
+import shutil
 from concert.models import GigGoer, User, Venue, Concert
 from django.db import models 
 from django.contrib.auth.hashers import make_password
@@ -13,6 +14,7 @@ class Populate():
     def __init__(self):
         self.populate_giggoers()
         self.populate_venues()
+        self.populate_concerts()
 
     def populate_giggoers(self):
         path = os.path.join(os.getcwd(), 'population_files', 'giggoers.csv')
@@ -20,7 +22,7 @@ class Populate():
         print(data)
         for ir in data.itertuples():
             imgpath = os.path.join(os.getcwd(), 'population_files', 'images', str(ir[4]) + ".jpg")
-            os.rename(imgpath, os.path.join(os.getcwd(), 'media', 'profile_images', str(ir[4]) + ".jpg"))
+            shutil.copyfile(imgpath, os.path.join(os.getcwd(), 'media', 'profile_images', str(ir[4]) + ".jpg"))
             print(ir)
             print(ir[1])
             user = User.objects.create(
@@ -40,7 +42,7 @@ class Populate():
         print(data)
         for ir in data.itertuples():
             imgpath = os.path.join(os.getcwd(), 'population_files', 'images', str(ir[4]) + ".jpg")
-            os.rename(imgpath, os.path.join(os.getcwd(), 'media', 'venue_images', str(ir[4]) + ".jpg"))
+            shutil.copyfile(imgpath, os.path.join(os.getcwd(), 'media', 'venue_images', str(ir[4]) + ".jpg"))
             print(ir)
             print(ir[1])
             
@@ -61,9 +63,29 @@ class Populate():
 
             user.save()
             venue.save()
-            
+
+    def populate_concerts(self):
+        path = os.path.join(os.getcwd(), 'population_files', 'concerts.csv')
+        data = pd.read_csv(path)
 
 
+        for ir in data.itertuples():
+            print(ir)
+            imgpath = os.path.join(os.getcwd(), 'population_files', 'images', str(ir[5]) + ".jpg")
+            shutil.copyfile(imgpath, os.path.join(os.getcwd(), 'media', 'concert_images', str(ir[5]) + ".jpg"))
+
+
+            concert = Concert.objects.create(
+            artist = ir[1],
+            date   = ir[2],
+            start_time = ir[3],
+            end_time = ir[4],
+            image = os.path.join(os.getcwd(), 'media', 'concert_images', str(ir[5]) + ".jpg"),
+            url = ir[6],
+            description = ir[7],
+            venue = User.objects.get(username=ir[8]).venue
+            )
+            concert.save()
 
 if __name__ == "__main__":
   Populate()
