982cdcef615c58d12755c9028b06bae9f1fb1b73
Patryk K <patryk.kaczmarczyk.mail@gmail.com>
2019-03-16 20:13:32 +0000

    Implemented bootstrap datepicker and timepicker for concert forms

--- FindMyConcert/settings.py
+++ FindMyConcert/settings.py
@@ -42,7 +42,8 @@ INSTALLED_APPS = [
     'concert',
     'venues',
     'crispy_forms',
-
+    'bootstrap4',
+    'bootstrap_datepicker_plus',
 ]
 
 MIDDLEWARE = [
--- venues/forms.py
+++ venues/forms.py
@@ -1,12 +1,19 @@
 from concert.models import Concert
+from bootstrap_datepicker_plus import DatePickerInput, TimePickerInput
 from django import forms
 
+class DateInput(forms.DateInput):
+    input_type = "date"
+
+class TimeInput(forms.TimeInput):
+    input_type = "time"
+
 class ConcertForm(forms.ModelForm):
-    artist      = forms.CharField(max_length=128, )
-    date        = forms.DateField( input_formats=['%d/%m/%Y'])
-    start_time  = forms.TimeField( widget=forms.TimeInput(format='%H:%M'))
-    end_time    = forms.TimeField(widget=forms.TimeInput(format='%H:%M'))
-    image       = forms.ImageField( required = False)
+    artist      = forms.CharField(max_length=128)
+    date        = forms.DateField(widget=DatePickerInput(format="%d/%m/%Y"))
+    start_time  = forms.TimeField(widget=TimePickerInput(format="%H:%M"))
+    end_time    = forms.TimeField(widget=TimePickerInput(format="%H:%M"))
+    image       = forms.ImageField(required = False)
     url         = forms.URLField()
     description = forms.CharField(max_length=1000)
 
@@ -21,11 +28,11 @@ class DeleteConcertForm(forms.ModelForm):
 
 class EditConcertForm(forms.ModelForm):
     artist      = forms.CharField(max_length=128, required = False)
-    date        = forms.DateField( input_formats=['%d/%m/%Y'], required = False)
-    start_time  = forms.TimeField( widget=forms.TimeInput(format='%H:%M'), required = False)
-    end_time    = forms.TimeField(widget=forms.TimeInput(format='%H:%M'), required = False)
-    image       = forms.ImageField( required = False)
-    url         = forms.URLField( required = False)
+    date        = forms.DateField(widget=DatePickerInput(format="%d/%m/%Y"), required = False)
+    start_time  = forms.TimeField(widget=TimePickerInput(format="%H:%M"), required = False)
+    end_time    = forms.TimeField(widget=TimePickerInput(format="%H:%M"), required = False)
+    image       = forms.ImageField(required = False)
+    url         = forms.URLField(required = False)
     description = forms.CharField(max_length=1000, required = False)
     class Meta:
         model = Concert
