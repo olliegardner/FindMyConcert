a8b47ba21f411421c60d644e8379f7f903cbfbc4
“2304777P” <2304777P@student.gla.ac.uk>
2019-03-17 11:31:35 +0000

    Fixing more merge conf

--- concert/views.py
+++ concert/views.py
@@ -181,10 +181,6 @@ def bookmark(request):
 @login_required
 @giggoer_required
 def removeBookmark(request):
-<<<<<<< HEAD
-    print("Starting to remove bookmark")
-=======
->>>>>>> master
     concert_to_remove = None
 
     if request.method == 'GET':
@@ -287,11 +283,7 @@ def postComment(request):
     text = request.POST.get('data')
     concertID = request.POST.get('id')
     if text == "" or text == None:
-<<<<<<< HEAD
-        payload = {'success': False}
-=======
         payload = {'success': "False"}
->>>>>>> master
     else:
         concert = get_object_or_404(Concert, concertID=concertID)
         comment = Comment.objects.create(
@@ -303,11 +295,7 @@ def postComment(request):
             image = user.venue.image.url
         else:
             image = user.giggoer.image.url
-<<<<<<< HEAD
-        payload = {'success': True, 'username':user.username, 'image':image}
-=======
         payload = {'success': "True", 'username':user.username, 'image':image}
->>>>>>> master
         comment.save()
 
     return HttpResponse(json.dumps(payload), content_type='application/json')
