2822e81cd72bbc3e75623d82e4308f8998261f52
olliegardner <olliegardner@gmail.com>
2019-03-20 16:29:12 +0000

    now a smooth animation, not working on safari

--- static/js/events.js
+++ static/js/events.js
@@ -1,8 +1,8 @@
 // when the user scrolls down, show the button
-window.onscroll = function() { onScroll() };
+/*window.onscroll = function() { onScroll() };
 
 function onScroll() {
-	if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+	if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
     	document.getElementById("go-top").style.display = "block";
   	} else {
     	document.getElementById("go-top").style.display = "none";
@@ -13,4 +13,20 @@ function onScroll() {
 function goTop() {
 	document.body.scrollTop = 0; // Safari
   	document.documentElement.scrollTop = 0; // Chrome, Firefox, IE and Opera
-}
+}*/
+
+$(document).ready(function() { 
+    $(window).scroll(function() { 
+        if ($(this).scrollTop() > 100) { 
+            $('#go-top').fadeIn(); 
+        } else { 
+            $('#go-top').fadeOut(); 
+        } 
+    }); 
+
+    $('#go-top').click(function(){ 
+        $("html, body").animate({ scrollTop: 0 }, 300); 
+        return false; 
+    }); 
+});
+
