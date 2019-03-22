1dc9a1307d386d737206564617106d1d81f45b3a
olliegardner <olliegardner@gmail.com>
2019-03-22 12:36:54 +0000

    concert_getter changes

--- concert_getter.py
+++ concert_getter.py
@@ -9,28 +9,37 @@ from datetime import date, datetime, timedelta
 from django.contrib.staticfiles.templatetags.staticfiles import static
 from django.db import IntegrityError
 from secret.secret_settings import SONG_KICK_API_KEY
+from django.core.files import File
 
 import requests
 import sys
 import random
 
-def get_create_venue(username, venue_nam, location, website, description, phone_number, capacity, image):
+imgpath = os.path.join(os.getcwd(), 'static', 'images', "default-pic.png")
+
+def get_create_venue(username, venue_name, location, website, description, phone_number, capacity):
     email = username + "@" + username + ".com"
 
-    try:
-        user, new_user = User.objects.get_or_create(username=username,  email=email, password='strongpass123', is_venue=True)
-        venue, new_venue = Venue.objects.get_or_create(user=user, venue_name=venue_name, location=location, website=website, description=description, phone_number=phone_number, capacity=capacity, image=image)
-        return venue
-    except IntegrityError:
-        print('User already exists. Try another artist!')
-        exit(1)
+    user = User.objects.create(username=username, email=email, password='strongpass123', is_venue=True)
+    #imgpath = os.path.join(os.getcwd(), 'static', 'images', "default-pic.png")
+    #image = ('default', File(open(imgpath, 'rb')))
+    venue = Venue.objects.create(
+        user=user,
+        venue_name=venue_name,
+        location=location, 
+        website=website, 
+        description=description, 
+        phone_number=phone_number, 
+        capacity=capacity)
+    venue.image.save('default', File(open(imgpath, 'rb')))
+    venue.save()
 
 
 if len(sys.argv) >= 2:
     input_artist = ' '.join(sys.argv[1:])
     current_date = date.today()
-    start_date = str(current_date.replace(year=current_date.year-1))
-    end_date = str(current_date.replace(year=current_date.year+1))
+    start_date = str(current_date.replace(year=current_date.year-5))
+    end_date = str(current_date.replace(year=current_date.year+5))
 
     # returns songkick event objects with artist name with a 2 year range
     url = 'https://api.songkick.com/api/3.0/events.json?apikey=' + SONG_KICK_API_KEY + '&artist_name=' + input_artist + '&min_date=' + start_date + '&max_date=' + end_date
@@ -38,33 +47,41 @@ if len(sys.argv) >= 2:
     response = requests.get(url=url)
     data = response.json()
 
+    #try:
     concerts = data['resultsPage']['results']['event'] # gets list of concerts from dictionary
     random_concert = random.choice(concerts) # chooses random concert from list
 
     artist = random_concert['performance'][0]['displayName']
-
     d = random_concert['start']['date']
-    #new_date = datetime.strptime(d, '%Y-%m-%d') # converts string back to datetime object
-    #new_date = new_date.strftime('%d/%m/%Y') # converts date to right format for adding to concert object
 
     start_time = random_concert['start']['time']
-    end_time = datetime.strptime(start_time, '%H:%M:%S') + timedelta(hours=2) # songkick api does not have end time so manually add 2 hours
-    end_time = end_time.strftime('%H:%M:%S')
-    image = static('note.png')
+    end_time = None
+
+    if start_time != None:
+        end_time = datetime.strptime(start_time, '%H:%M:%S') + timedelta(hours=2) # songkick api does not have end time so manually add 2 hours
+        end_time = end_time.strftime('%H:%M:%S')
+    else:
+        start_time = "19:00:00"
+        end_time = "22:00:00"
+
     url = random_concert['uri']
     description = random_concert['displayName']
 
     venue_name = random_concert['venue']['displayName']
     location = random_concert['location']['city']
 
+    venue = get_create_venue(venue_name.lower().replace(' ', '_'), venue_name, location, url, description, 123456789, 69)
 
-    venue = get_create_venue(venue_name.lower().replace(' ', '_'), venue_name, location, url, description, 123456789, 69, image)
+    #image = ('default', File(open(imgpath, 'rb')))
 
-    try:
-        concert, new_concert = Concert.objects.get_or_create(artist=artist, date=d, start_time=start_time, end_time=end_time, image=image, url=url, description=description, spotify_URI='hgvhvhvh', venue=venue)
-        print("Concert created!")
-    except IntegrityError:
-        print('ERROR: This concert already exists! Try a different artist.')
+    concert = Concert.objects.create(artist=artist, date=d, start_time=start_time, end_time=end_time, url=url, description=description, spotify_URI='hgvhvhvh', venue=venue)
+    concert.image.save('default', File(open(imgpath, 'rb')))
+    concert.save()
+    print("Concert created!")
+    
+    #except KeyError:
+        #print('No results')
+        #exit(1)        
 
 else:
     print('Format: python concert_getter.py [artist]')
