21063b68f040987ded10978c0a9a4eaccc20231c
Ollie <olliegardner@gmail.com>
2019-06-06 17:25:46 +0100

    removed song kick

--- secret/secret_settings.py
+++ secret/secret_settings.py
@@ -2,4 +2,3 @@
 # this will be added to .gitignore once project is graded
 GOOGLE_MAPS_API_KEY = "AIzaSyA80wEVfNDR49oTo3yede1S6Nik_Lw6PyY"
 EMAIL_PASSWORD = "bw38k394"
-SONG_KICK_API_KEY = "XKMdS7NjLNYNen3B"
